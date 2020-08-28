ce7ffe00fb36e14297b17f54dfdf42f425c39d57
ali <alisamymohamed@yahoo.com>
2020-08-27 19:06:33 -0700

    Initial commit

---
+++ app/src/main/java/com/ali/marvelapp/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ali.marvelapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ali.marvelapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
